053c08ddef1815c1dcb05da0ac62a495ce4d6369
TMaksyutov <dadojkeee@mail.ru>
2023-01-02 13:21:32 +0300

    added TestBase

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,15 @@
+package tests;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+import static helpers.CustomApiListener.withCustomTemplates;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.filters(withCustomTemplates());
+    }
+
+}
