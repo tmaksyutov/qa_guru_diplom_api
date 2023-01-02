86f37c9a16d8a8efe85dbafc26d214fb716ac766
TMaksyutov <dadojkeee@mail.ru>
2023-01-02 13:21:09 +0300

    added tests

---
+++ src/test/java/tests/ReqresInTests.java
@@ -0,0 +1,147 @@
+package tests;
+
+import models.LombokUserData;
+import models.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static spec.Specs.request;
+import static spec.Specs.responseSpec200;
+
+public class ReqresInTests extends TestBase {
+
+    @Test
+    @DisplayName("Checking email with Groovy")
+    void checkEmailWithGroovy() {
+        given()
+                .spec(request)
+                .when()
+                .get("/users")
+                .then()
+                .spec(responseSpec200)
+                .log().body()
+                .body("data.findAll{it.email =~/.*?@reqres.in/}.email.flatten()",
+                        hasItems("eve.holt@reqres.in"));
+    }
+
+    @Test
+    @DisplayName("Successful registration")
+    void successfulRegister() {
+
+        User user = new User();
+        user.setEmail("eve.holt@reqres.in");
+        user.setPassword("pistol");
+
+        User response = given()
+                .spec(request)
+                .body(user)
+                .when()
+                .post("/register")
+                .then()
+                .spec(responseSpec200)
+                .log().body()
+                .extract().as(User.class);
+
+        assertEquals("4", response.getId());
+        assertEquals("QpwL5tke4Pnpja7X4", response.getToken());
+    }
+
+    @Test
+    @DisplayName("Successful authorization")
+    void successfulLogin() {
+        User user = new User();
+        user.setEmail("eve.holt@reqres.in");
+        user.setPassword("cityslicka");
+
+        User response = given()
+                .spec(request)
+                .body(user)
+                .when()
+                .post("/login")
+                .then()
+                .spec(responseSpec200)
+                .log().body()
+                .extract().as(User.class);
+
+        assertEquals(response.getToken(), "QpwL5tke4Pnpja7X4");
+    }
+
+    @Test
+    @DisplayName("Unsuccessful authorization")
+    void unsuccessfulLogin() {
+        User user = new User();
+        user.setEmail("peter@klaven");
+
+        User response = given()
+                .spec(request)
+                .body(user)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(400)
+                .log().body()
+                .extract().as(User.class);
+
+        assertEquals(response.getError(), "Missing password");
+    }
+
+    @Test
+    @DisplayName("Create a new user")
+    void createUser() {
+        User user = new User();
+        user.setName("morpheus");
+        user.setJob("leader");
+
+        User response = given()
+                .spec(request)
+                .body(user)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(201)
+                .log().body()
+                .extract().as(User.class);
+        assertEquals(response.getName(), user.getName());
+        assertEquals(response.getJob(), user.getJob());
+    }
+
+    @Test
+    @DisplayName("Update user data")
+    void updateUser() {
+        User user = new User();
+        user.setName("morpheus");
+        user.setJob("zion resident");
+
+        User response = given()
+                .spec(request)
+                .body(user)
+                .when()
+                .put("/users/2")
+                .then()
+                .spec(responseSpec200)
+                .log().body()
+                .extract().as(User.class);
+        assertEquals(response.getName(), user.getName());
+        assertEquals(response.getJob(), user.getJob());
+    }
+
+    @Test
+    @DisplayName("Search user")
+    void singleUser() {
+
+        LombokUserData response = given()
+                .spec(request)
+                .when()
+                .get("/users/2")
+                .then()
+                .spec(responseSpec200)
+                .log().body()
+                .extract().as(LombokUserData.class);
+
+        assertEquals("2", response.getUser().getId());
+        assertEquals("janet.weaver@reqres.in", response.getUser().getEmail());
+    }
+}
